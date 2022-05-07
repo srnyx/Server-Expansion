ebf8a3194fd3ea46b47255e4858af82d572c26f0
srnyx <25808801+srnyx@users.noreply.github.com>
2022-05-06 21:31:35 -0400

    Switch to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+description = "PAPI-Expansion-Server"
+version = "2.6.2"
+group = "com.extendedclip.papi.expansion.server"
+
+repositories {
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+dependencies {
+    compileOnly("me.clip:placeholderapi:2.10.11")
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    implementation("com.github.ben-manes.caffeine:caffeine:1.3.3")
+}
+
+plugins {
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+}
